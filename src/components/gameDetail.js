d745356b4464759f72eecd6ff23a0c34ea78f88c
drood87 <dbernstein1987@hotmail.com>
2019-06-22 14:09:04 +0800

    new query to filter

--- src/components/gameDetail.js
+++ src/components/gameDetail.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import Layout from './layout';
 
-export default class gameDetail extends Component {
+export default class GameDetail extends Component {
+  componentDidMount() {}
+
   render() {
+    const { allInternalData } = this.props;
     return (
-      <div>
-        <h1>Hello From here</h1>
-      </div>
+      <Layout>
+        <h1>{allInternalData.edges[0].node.published.slug}</h1>
+      </Layout>
     );
   }
 }
+
+export const query = graphql`
+  query GameQuery($slug: String!, $id: Int!, $name: String!) {
+    allInternalData(filter: { id: { ne: "dummy" } }) {
+      edges {
+        node {
+          published {
+            slug
+            alternative_id
+            name
+          }
+        }
+      }
+    }
+  }
+`;
+
+GameDetail.propTypes = {
+  allInternalData: PropTypes.shape({
+    edges: PropTypes.shape({
+      node: PropTypes.shape({
+        published: PropTypes.shape({
+          slug: PropTypes.string,
+          id: PropTypes.number,
+          name: PropTypes.string,
+        }).isRequired,
+      }).isRequired,
+    }).isRequired,
+  }).isRequired,
+};
