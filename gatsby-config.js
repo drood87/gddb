a0fdd638ffcc1a41f52a7dc3a563a98a9d30fc6d
drood87 <dbernstein1987@hotmail.com>
2019-06-24 13:40:30 +0800

    proxy api request

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
         typePrefix: 'internal__',
 
         // The url, this should be the endpoint you are attempting to pull data from
-        url: 'https://api-v3.igdb.com/companies',
+        url: 'https://crossorigin.me/https://api-v3.igdb.com/companies',
 
         method: 'POST',
 
