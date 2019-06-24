21b686a7a1f821272282c71a02f1132c80d28f54
drood87 <dbernstein1987@hotmail.com>
2019-06-24 13:53:57 +0800

    add netlify headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -91,6 +91,7 @@ module.exports = {
         icon: 'src/images/logo.svg', // This path is relative to the root of the site.
       },
     },
+    'gatsby-plugin-netlify',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
